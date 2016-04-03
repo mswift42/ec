ee3fdee5a2210d3d6dd17c61fae947f18cb66b90
mswift42 <martin.haesler@gmail.com>
2016-04-03 21:18:44 +0100

    Add obscure broccoli dependencies.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,18 @@
 /* global require, module */
 
 var Angular2App = require('angular-cli/lib/broccoli/angular2-app');
+var MergeTrees = require('broccoli-merge-trees');
+const BroccoliFunnel = require('broccoli-funnel');
 
 module.exports = function(defaults) {
   var app = new Angular2App(defaults, {
     vendorNpmFiles: []
   });
-  return app.toTree();
+  var MaterialTree = BroccoliFunnel('node_modules/@angular2-material', {
+    destDir: '@angular2-material'
+  });
+  return MergeTrees([
+      app.toTree(),
+      MaterialTree,
+  ])
 };
