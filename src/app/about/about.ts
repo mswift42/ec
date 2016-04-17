9463cce3553ad3a86cb2375a83af1a352f82ff49
mswift42 <martin.haesler@gmail.com>
2016-04-17 19:26:42 +0100

    Generate about component

---
+++ src/app/about/about.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'about',
+  templateUrl: 'app/about/about.html',
+  styleUrls: ['app/about/about.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class About {
+
+  constructor() {}
+
+}
